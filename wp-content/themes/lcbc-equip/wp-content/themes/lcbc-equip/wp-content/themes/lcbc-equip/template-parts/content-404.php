9509b297c2d8459a41a5f6f2e010987582dbb9f2
Emily C <echois03@gmail.com>
2023-03-01 18:01:13 -0800

    update cfc to equip description

---
+++ wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/template-parts/content-404.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function astra_entry_before() in /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/themes/lcbc-equip/template-parts/content-404.php:13
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/echois/Local Sites/lcbc-equip/app/public/wp-content/themes/lcbc-equip/template-parts/content-404.php</b> on line <b>13</b><br>
