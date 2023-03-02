e508e905606761fce4f62e701ba2a4f19a19fb0c
Emily C <echois03@gmail.com>
2023-03-01 16:03:19 -0800

    add all wp-content

---
+++ wp-content/plugins/custom-typekit-fonts/classes/class-typekit-fonts-white-label.php
@@ -0,0 +1,7 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function add_filter() in /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/plugins/custom-typekit-fonts/classes/class-typekit-fonts-white-label.php:56
+Stack trace:
+#0 /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/plugins/custom-typekit-fonts/classes/class-typekit-fonts-white-label.php(44): Typekit_Fonts_White_Label-&gt;__construct()
+#1 /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/plugins/custom-typekit-fonts/classes/class-typekit-fonts-white-label.php(192): Typekit_Fonts_White_Label::set_instance()
+#2 {main}
+  thrown in <b>/Users/echois/Local Sites/lcbc-equip/app/public/wp-content/plugins/custom-typekit-fonts/classes/class-typekit-fonts-white-label.php</b> on line <b>56</b><br>
---
+++ wp-content/plugins/custom-typekit-fonts/templates/custom-typekit-fonts-options.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function get_option() in /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/plugins/custom-typekit-fonts/templates/custom-typekit-fonts-options.php:8
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/echois/Local Sites/lcbc-equip/app/public/wp-content/plugins/custom-typekit-fonts/templates/custom-typekit-fonts-options.php</b> on line <b>8</b><br>
