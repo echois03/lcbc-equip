de4a9cf3e774aeb64691b7afe91f207236e25677
Emily C <echois03@gmail.com>
2023-03-02 17:55:04 -0800

    updated info

---
+++ wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/wp-content/themes/astra/inc/compatibility/starter-content/home.php
@@ -0,0 +1,5 @@
+<br>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function trailingslashit() in /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/themes/astra/inc/compatibility/starter-content/home.php:24
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/echois/Local Sites/lcbc-equip/app/public/wp-content/themes/astra/inc/compatibility/starter-content/home.php</b> on line <b>24</b><br>
---
+++ wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/wp-content/themes/lcbc-equip/template-parts/single/single-layout.php
@@ -0,0 +1,5 @@
+<div></div>
+<b>Fatal error</b>:  Uncaught Error: Call to undefined function astra_blog_layout_class() in /Users/echois/Local Sites/lcbc-equip/app/public/wp-content/themes/lcbc-equip/template-parts/single/single-layout.php:14
+Stack trace:
+#0 {main}
+  thrown in <b>/Users/echois/Local Sites/lcbc-equip/app/public/wp-content/themes/lcbc-equip/template-parts/single/single-layout.php</b> on line <b>14</b><br>
